ce46ab36efada42c01ec9cb42a9e8721a9603fd7
Sergio Lillo <sergio.lillo@rethinkmarketing.es>
2022-04-19 11:46:52 +0200

    Add project files.

---
+++ XamarinFormsLifeCycleIssue/XamarinFormsLifeCycleIssue/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsLifeCycleIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+    }
+}
